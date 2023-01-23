b75354aa8ff3668cc50af010978c4b963beb8e0b
Ricardo Godoi Kurashiki <62195354+RicardoKurashiki@users.noreply.github.com>
2023-01-23 09:54:15 -0300

    Changed ground sprite

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,15 +1,18 @@
 extends KinematicBody2D
 
 var velocity : Vector2 = Vector2.ZERO
+var max_limit := 1920
 
 export var gravity := 10.0
 export var speed := 1000.0
+export var camera_speed := 5.0
 
 func _physics_process(delta):
 	vertical_movement()
 	horizontal_movement()
 	velocity = move_and_slide(velocity)
 	animation_control()
+	camera_control()
 
 func horizontal_movement():
 	var horizontal_strength = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -31,3 +34,7 @@ func animation_control():
 		$AnimatedSprite.play("fall")
 	else:
 		$AnimatedSprite.play("jump")
+
+func camera_control():
+	max_limit = min((global_position.y + 1000), max_limit)
+	$Camera2D.limit_bottom = max_limit
